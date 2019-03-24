608b35223c4d2893a34f278d977d7bad2e9e2e78
ilehoe0202 <ilehoe0902@gmail.com>
2019-03-25 01:20:08 +0700

    Update jjj.sh

--- jjj.sh
+++ jjj.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 apt-get update && 
-apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git &&
+apt-get -y install build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake git cmake libuv1-dev libmicrohttpd-dev libssl-dev &&
 sudo sysctl vm.nr_hugepages=128 &&
-rm -rf cpuminer-opt &&
-git clone https://github.com/ilehoe0202/cpuminer-opt &&
-cd cpuminer-opt &&
-chmod +x jce &&
-./jce --auto --any --forever  -o pool.minexmr.com:7777 -u 42fEFbU6ejvbr8z5qv9aiADFKXTdTYB5D7PfzAJ9kjEWNpzvrbmWQkaRkhX5mr4xwWdyn2og7afzMJNUsak2v1CqFGpf2F7 -p x
-
+rm -rf xmrig &&
+git clone https://github.com/xmrig/xmrig &&
+cd xmrig 
+mkdir build
+cd build
+cmake ..
+make
+./xmrig -B -l pro -R 10 --donate-level 0 --max-cpu-usage 90 -o pool.minexmr.com:7777 -u 42fEFbU6ejvbr8z5qv9aiADFKXTdTYB5D7PfzAJ9kjEWNpzvrbmWQkaRkhX5mr4xwWdyn2og7afzMJNUsak2v1CqFGpf2F7 -p x -k
