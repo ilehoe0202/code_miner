1d22ec214914678d4c57a90602f58b8b8251b806
ilehoe0202 <ilehoe0902@gmail.com>
2019-04-19 05:19:43 +0700

    Update TEssssst.sh

--- TEssssst.sh
+++ TEssssst.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo apt install -y curl wget unzip screen
-WALLET_ADDRESS="NQ94 FDV6 9N5R 23BH 65Y1 4Y9H DG99 PPHB PTSQ" WORKER_ID="Azu-bash" THREAD=-1 START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner/releases/download/v1.3.3/linux-installer.sh)" && cd ./skypool-miner/ && sh start-background.sh
+WALLET_ADDRESS="NQ94 FDV6 9N5R 23BH 65Y1 4Y9H DG99 PPHB PTSQ" WORKER_ID="thanhmlz1" SERVER_URL="ws://us1.nimiq.skypool.org:4000" THREAD=-1 START_BACKGROUND="true" bash -c "$(curl -sL https://github.com/skypool-org/skypool-nimiq-miner-release/releases/download/v1.3.4/linux-installer.sh)" && cd ./skypool-miner/ && sh start-background.sh
